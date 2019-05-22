4309019038a6a44067d2f7eea1711624f31a2f4f
MOMOKO606 <bianlong0618@qq.com>
2019-05-22 17:20:58 +0800

    Rewrite functions into module.

--- Test/TpTracker.py
+++ Test/TpTracker.py
@@ -199,13 +199,14 @@ class TpTrackers():
             return 15
 
 
-    def FilterClsTable( self, st, ft, Dep, cyconsts, teachers ):
+    def FilterClsTable( self, st, ft, Dep, cyconsts, teachers, output_mode ):
         """
         function: 根据参数，计算一段时间内，某部门（或全部），某课程（或全部）的产能，与各教师在其中的产能列表。
         :param st & ft: 起始 & 终止时间范围；Dep: 项目部门； cyconsts: 课程类型； teachers: 教师姓名。
-        :return: resclstab：梳理统计后的简化班表，teacherlist：教师产能列表。
-                输出2个txt文档：Summary.txt 和 Classes_table.txt，即resclstab的内容；
-                输出1个excel文档：Teacher_List.xlsx，即teacherlist的内容。
+               output_mode: 是否输出txt和excel文件。
+        :return: summary_text：梳理统计后的综述，teacherdict（教师产能列表）：[姓名，总产能，总课时，产能/课时，产能/月，工作月份]。
+                 输出2个txt文档：Summary.txt 和 Classes_table.txt，即resclstab的内容；
+                 输出1个excel文档：Teacher_List.xlsx，即teacherdict的内容。
         """
 
         #  载入类属性值。
@@ -228,7 +229,7 @@ class TpTrackers():
         #  赋初值：
         resclstab = []  # Result Class Table.
         #  用于计算的classes dictionary.
-        #  班级编号为key，value为一个dict{该班级的所有老师名：其对应的上课次数}。
+        #  班级编号为key，value为一个dict{该班级的所有老师名：其对应的上课时长}。
         classes = {}
         header = table.row_values(0)  # 第一行为表头。
         for i in range( 1, rows ):
@@ -240,7 +241,8 @@ class TpTrackers():
 
             #  Sentinels
             #  Teacher name Constraint.
-            if curteacher not in teachers and len(teachers) >= 1:
+            #  例如teachers=“刘莹”，curteacher=“刘莹-雅思阅读”，则为合法数据。
+            if teachers not in curteacher and len(teachers) >= 1:
                 continue
             #  Date Constraint.
             if curdate < startdate or curdate > enddate:
@@ -293,7 +295,7 @@ class TpTrackers():
         #  (2).以教师为primary key，整理&修改classes和resclstab。
         totalclsn = len(resclstab)  # Total classes number.
         totalfee = 0  # 总业绩初值。
-        teacherlist = {}  # 参与教师dict初值。
+        teacherdict = {}  # 参与教师dict初值。
 
         for i in range(totalclsn):
             clsnum = resclstab[i][2]
@@ -315,37 +317,81 @@ class TpTrackers():
 
             #  更新该课程中每位老师的课数、课时贡献百分比 和 对应的完成业绩。
             for t in classes[clsnum].keys():
-                #  首次统计到该老师时，将其姓名加入teacherlist。
-                if t not in teacherlist.keys():
-                    teacherlist[t] = [0, 0, 0]
+                #  首次统计到该老师时，将其姓名加入teacherdict。
+                if t not in teacherdict.keys():
+                    #  教师姓名t：总产能，总课时，产能/小时，产能/月，最早上课日期，最晚上课日期, month_rate。
+                    teacherdict[t] = [0, 0, 0, 0, None, None, None]
                 time = classes[clsnum][t]  # t老师在该班级上了time分钟的课。
                 p = time / tct
                 #  更新classes，存入t老师在该班级上了times课，占所有课次的比例，以及对应该班级学费的业绩。
-                classes[clsnum][t] = [time, round(p, 2), round(p * fee, 2)]
+                classes[clsnum][t] = [time, round(p, 5), round(p * fee, 2)]
             #  更新resclstab中的classes信息。
             resclstab[i].append(classes[clsnum])
         # 参与教师总数。
-        totaltn = len(teacherlist.keys())
+        totaltn = len(teacherdict.keys())
 
         #  STEP5. 计算教师产值列表。
-        #  按每个班级中的每个教师的产能，叠加到teacherlist中该教师的总产能。
+        #  按每个班级中的每个教师的产能，叠加到teacherdict中该教师的总产能。
         for c in classes.keys():
             for t in classes[c].keys():
                 individ_time = classes[c][t][0]  # 个人时长。
                 individ_performance = classes[c][t][2]  # 个人业绩。
-                teacherlist[t][0] += round( individ_performance, 2 )  # 累积个人业绩。
-                teacherlist[t][1] += round(individ_time/60, 2)  # 累积个人课时（单位：小时）。
+                teacherdict[t][0] += individ_performance  # 累积个人业绩。
+                teacherdict[t][1] += round(individ_time/60, 2)  # 累积个人课时（单位：小时）。
 
         # 更新产能课时比。
-        for t in teacherlist.keys():
-            teacherlist[t][2] = round(teacherlist[t][0] / teacherlist[t][1], 2)
+        for t in teacherdict.keys():
+            teacherdict[t][0] = round(teacherdict[t][0], 2)
+            teacherdict[t][2] = round(teacherdict[t][0] / teacherdict[t][1], 2)  # 更新每学时产能。
+
+            #  在班表中查找t老师上课的最早日期和最晚日期。
+            #  对于最早和最晚日期赋初值。
+            earliest = datetime.datetime.strptime("2200-01-01", "%Y-%m-%d")
+            latest = datetime.datetime.strptime("1880-01-01", "%Y-%m-%d")
+            for i in range( 1, rows ):
+                r = table.row_values(i)
+                curdate = xlrd.xldate_as_datetime(r[self.CLASS_DATE_POS], 0)  # 当前日期。
+                curteacher = r[self.TEACHERS_POS]  # 当前教师姓名。
+                curdep = r[self.DEPT_POS]  # 当前部门。
+                curcls = r[self.CLASS_POS]  # 当前课程种类。
+                #  Sentinels.
+                #  不是t老师则跳过。
+                if curteacher != t:
+                    continue
+                #  Date Constraint.
+                if curdate < startdate or curdate > enddate:
+                    continue
+                #  Department Constraint.
+                if len(Dep) >= 1:  # 没有使用default的情况。
+                    if Dep not in curdep:
+                        continue
+                #  Class Type Constraint.
+                if len(cyconsts) >= 1:  # 没有使用default的情况。
+                    flag = self.isinClassType(self.CLASS_TYPE, cyconsts, curcls, r[self.STU_NUM_POS])
+                    if not flag:
+                        continue
+                #  排除自习课、辅导课、模考等。
+                if len(r[self.TEACHERS_POS]) == 0:
+                    continue
+                #  排除没学费的课程。
+                if r[self.FEE_POS] == "#N/A":
+                    continue
+                if curdate < earliest:
+                    earliest = curdate
+                if curdate > latest:
+                    latest = curdate
+            # month_rate为[startdate, enddate]期间带课的月份。
+            month_rate = round((latest - earliest).days / 30, 2)
+            teacherdict[t][3] = round(teacherdict[t][0] / month_rate, 2)  #  更新每月产能。
+            teacherdict[t][4] = earliest
+            teacherdict[t][5] = latest
+            teacherdict[t][6] = month_rate
 
         #  排序教师产值.
-        #  对teacherlist（dict）的value排序。
-        teacherlist = sorted(teacherlist.items(), key=lambda item: item[1], reverse=True)
+        #  对teacherdict（dict）的value排序。
+        teacherdict = sorted(teacherdict.items(), key=lambda item: item[1], reverse=True)
 
-        #  STEP6. 输出3个结果文件。
-        #  输出结果文件1：“Summary.txt”.
+        #  STEP6.创建返回值output_text和teacherlist。
         if len(Dep) < 1:
             Dep = "全部项目部"
         if len(cyconsts) < 1:
@@ -364,56 +410,194 @@ class TpTrackers():
                        + "********************************************"
                        + "\n"
                        + "\n")
-
-        f = open("Summary.txt", "a+")  # 如果存在该文件则在末尾添加，否则就创建该文件。
-        f.write(output_text)
-        f.close()
-
-        #  输出结果文件2：“Classes_Table.txt”.
-        f = open("Classes_Table.txt", "a+")  # 如果存在该文件则在末尾添加，否则就创建该文件。
-        for i in range(len(resclstab)):
-            f.writelines(str(resclstab[i]) + "\n")
-        f.write(("********************************************"
-                       + "\n"
-                       + "\n"))
-        f.close()
-
-        #  输出结果文件3：“Teacher_List.xlsx”.
-        #  获取当前路径，并查看Teacher_List.xlsx是否存在。
-        cwd = os.getcwd()
-        #  文件存在，则新建sheet。
-        if os.path.exists(cwd + "\Teacher_List.xlsx"):
-            wb = load_workbook("Teacher_List.xlsx")
-            ws = wb.create_sheet()  #  默认插在末尾。
-        #  文件不存在则新建。
-        else:
-            #  在内存中创建一个workbook对象，而自动创建一个worksheet。
-            wb = Workbook()
-            #  获取当前活跃的worksheet，默认就是第一个worksheet。
-            ws = wb.active
-        #  写入excel。
-        ws.append(["教师姓名", "总产能", "总课时", "产能/课时"])
+        #  赋初值。
+        teacherlist = []
+        perform_mean = 0
+        hours_mean = 0
+        pperh_mean = 0
+        pperm_mean = 0
+        month_mean = 0
         for i in range(totaltn):
             #  复杂的dict无法直接输出到excel，先转换成list。
-            tmp = [teacherlist[i][0], teacherlist[i][1][0], teacherlist[i][1][1], teacherlist[i][1][2]]
-            ws.append(tmp)
-        #  保存excel。
-        wb.save("Teacher_List.xlsx")
+            teacher_name = teacherdict[i][0]  # 教师姓名。
+            perform_sum = teacherdict[i][1][0]  # 总产能。
+            hours_sum = teacherdict[i][1][1]  # 总课时。
+            pperh = teacherdict[i][1][2]  # 产能/课时。
+            pperm = teacherdict[i][1][3]  # 产能/月。
+            earliest_str = teacherdict[i][1][4].strftime("%Y-%m-%d")  # 带课的最早日期。
+            latest_str = teacherdict[i][1][5].strftime("%Y-%m-%d")  # 带课的最晚日期。
+            month_rate = teacherdict[i][1][6]
+
+            perform_mean += perform_sum
+            hours_mean += hours_sum
+            pperh_mean += pperh
+            pperm_mean += pperm
+            month_mean += month_rate
+            teacherlist.append([teacher_name,perform_sum,hours_sum,pperh,pperm,earliest_str,latest_str, month_rate])
+        #  teachlist不为空才进行总结。
+        if totaltn > 0:
+            teacherlist.append([" ", " ", " ", " ", "", " ", " ", " "])
+            teacherlist.append([" ", "平均产能", "平均课时", "平均产能/课时", "平均产能/月", " ", " ", "平均授课时间（月）"])
+            teacherlist.append([" ", round(perform_mean/totaltn), round(hours_mean/totaltn), round(pperh_mean/totaltn), round(pperm_mean/totaltn), " ", " ", round(month_mean/totaltn)])
+
+        #  STEP7. 创建3个输出文件。
+        #  输出结果文件1：“Summary.txt”.
+        if output_mode:
+            f = open("Summary.txt", "a+")  # 如果存在该文件则在末尾添加，否则就创建该文件。
+            f.write(output_text)
+            f.close()
+
+            #  输出结果文件2：“Classes_Table.txt”.
+            f = open("Classes_Table.txt", "a+")  # 如果存在该文件则在末尾添加，否则就创建该文件。
+            for i in range(len(resclstab)):
+                f.writelines(str(resclstab[i]) + "\n")
+            f.write(("********************************************"
+                     + "\n"
+                     + "\n"))
+            f.close()
+
+            #  输出结果文件3：“Teacher_List.xlsx”.
+            #  获取当前路径，并查看Teacher_List.xlsx是否存在。
+            cwd = os.getcwd()
+            #  文件存在，则新建sheet。
+            if os.path.exists(cwd + "\Teacher_List.xlsx"):
+                wb = load_workbook("Teacher_List.xlsx")
+                ws = wb.create_sheet()  # 默认插在末尾。
+            #  文件不存在则新建。
+            else:
+                #  在内存中创建一个workbook对象，而自动创建一个worksheet。
+                wb = Workbook()
+                #  获取当前活跃的worksheet，默认就是第一个worksheet。
+                ws = wb.active
+            #  写入excel。
+            ws.append(["教师姓名","总产能","总课时","产能/课时","产能/月","首节课日期","末节课日期","带课时间（月）"])
+            for i in range(len(teacherlist)):
+                ws.append(teacherlist[i])
+            #  保存excel。
+            wb.save("Teacher_List.xlsx")
+        return teacherlist, output_text
 
 
-#  Script1. 试算2018.06.01-2019.05-31，所有部门，所有老师的产能。
+def CalcuAll( CLASS_SCHEDULE_FILE, output_mode ):
+    """
+    function: 试算2018.06.01-2019.05-31，所有部门，所有老师的产能。
+    :param CLASS_SCHEDULE_FILE: 班表名称。
+    :return: 输出2个txt, 1个excel文档。
+    """
 
-#  定义functions的parameters。
+    #  定义类函数所需的parameters。
+    st = str()
+    ft = str()
+    Dep = str()
+    cyconsts = str()
+    teachers = str()
+
+    #  创建Teacher's performance Trackers实例。
+    fy19 = TpTrackers( CLASS_SCHEDULE_FILE )
+    #  计算Teacher's performance。
+    return fy19.FilterClsTable(st, ft, Dep, cyconsts, teachers, output_mode)
+
+
+def PerfomafterEntry( CLASS_SCHEDULE_FILE, ENTRYTIME_FILE, timewindow, timelimit ):
+    """
+    function: 试算每位老师入职后timewindow时间内的产能。
+    :param CLASS_SCHEDULE_FILE: 班表名称。
+    :param ENTRYTIME_FILE: 老师入职时间表。
+    :param timewindow: 要计算的入职后的时间范围（单位：月）。
+    :param timelimit: 要计算的开始带课后的时间范围（单位：月）。
+    :return entryperfom: 每位老师入职后timewindow时间内的产能列表。
+            [教师姓名，总产能，总课时，产能/课时，产能/月，首节课日期, 末节课日期, 带课时间（月），入职时间]
+    """
+
+    #  打开入职时间表。
+    data1 = xlrd.open_workbook( ENTRYTIME_FILE )
+    nametable = data1.sheets()[0]
+    m = nametable.nrows
+
+    #  定义常量。
+    TEACHER_POS = 2  #  教师姓名在excel中的第2列（从第0列开始）。
+    ENTRY_DATE_POS = 3  #  入职日期在excel中的第2列（从第0列开始）。
+    NOW_DATE = datetime.datetime.strptime("2019-05-21", "%Y-%m-%d")  #  入职至今，即“今”表示的日期。
+
+    #  创建Teacher's performance Trackers实例。
+    fy19 = TpTrackers( CLASS_SCHEDULE_FILE )
+
+    #  定义function所需的其他参数。
+    Dep = str()
+    cyconsts = str()
+
+    #  对结果赋初值。
+    entryperform =[]
+    #  对均值赋初值。
+    perform_mean = 0
+    hours_mean = 0
+    pperh_mean = 0
+    pperm_mean = 0
+    count = 0  # 新教师人数。
+    header = nametable.row_values(0)  # 第一行为表头。
+    for i in range(1, m):
+        r = nametable.row_values(i)
+        teachers = r[TEACHER_POS]    # 读取教师姓名。
+
+        #  读取入职时间。
+        entry_date = xlrd.xldate_as_datetime(r[ENTRY_DATE_POS], 0)
+        end_date = entry_date + datetime.timedelta(days=(int(timewindow) * 30))
+
+        #  datetime转str。
+        entry_date_str = entry_date.strftime("%Y-%m-%d")
+        end_date_str = end_date.strftime("%Y-%m-%d")
+
+        #  计算Teacher's performance。
+        [tmp, detail] = fy19.FilterClsTable(entry_date_str, end_date_str, Dep, cyconsts, teachers, False)
+
+        #  跳过空值的计算结果。
+        if len(tmp) < 1:
+            continue
+
+        delta = (NOW_DATE - entry_date).days
+        #  筛选入职不超过15个月，带课不超过12个月的新老师。
+        if delta < ((timewindow - 12) * 31 + 365) and tmp[0][7] <= 12:
+            tmp = list(tmp[0])  # 从[[]]修改为[]
+            tmp.append(entry_date.strftime("%Y-%m-%d"))
+            entryperform.append(tmp)
+            count += 1
+            perform_mean += tmp[1]
+            hours_mean += tmp[2]
+            pperh_mean += tmp[3]
+            pperm_mean += tmp[4]
+
+    entryperform.append([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    entryperform.append([" ", "平均产能", "平均课时", "平均产能/课时", "平均产能/月", " ", " ", " "])
+    entryperform.append([" ", round(perform_mean / count), round(hours_mean / count), round(pperh_mean / count),
+               round(pperm_mean / count), " ", " ", " "])
+
+    return entryperform
+
+
+#------------------------------------------------------------------
+#  Script:
+#  定义function的parameter。
 CLASS_SCHEDULE_FILE = "FY19国外配课表.xlsx"
-st = str()
-ft = str()
-Dep = str()
-cyconsts = str()
-teachers = str()
-#  创建Teacher's performance Trackers实例。
-fy19 = TpTrackers( CLASS_SCHEDULE_FILE )
-#  计算Teacher's performance。
-fy19.FilterClsTable( st, ft, Dep, cyconsts, teachers )
+ENTRYTIME_FILE = "【国外部】教师名单-3.7.xlsx"
+timewindow = 15
+timelimit = 12
+CASE = 1
 
+if CASE == 1:
+    #  test1. 试算2018.06.01-2019.05-31，所有部门，所有老师的产能。
+    CalcuAll( CLASS_SCHEDULE_FILE, True )
+elif CASE == 2:
+    # test3. 试算每位老师入职不超过timewindow个月，带课不超过timelimit个月的产能表现。
+    entryperform = PerfomafterEntry(CLASS_SCHEDULE_FILE, ENTRYTIME_FILE, timewindow, timelimit)
+    #  输出到excel。
+    #  在内存中创建一个workbook对象，而自动创建一个worksheet。
+    wb = Workbook()
+    #  获取当前活跃的worksheet，默认就是第一个worksheet。
+    ws = wb.active
+    ws.append(["教师姓名","总产能","总课时","产能/课时","产能/月","首节课日期","末节课日期","带课时间（月）","入职日期"])
+    for i in range(len(entryperform)):
+        ws.append(entryperform[i])
+    #  保存excel。
+    wb.save("Performance_after_entry.xlsx")
 
 
--- Test/test.py
+++ Test/test.py
@@ -1,4 +1,7 @@
-x = str()
+cur_name = "刘莹雅思"
+names = ["刘莹", "马豆丁", "花卷"]
 
+flagx = cur_name in names
+flagy = names in cur_name
 
-print(type(x), len(x))
+print("bianlong")
